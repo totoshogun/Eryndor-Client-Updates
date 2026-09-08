30b6be1f493b3c5f919adaeb9903d718ee1d3a9a
totoshogun <59440444+totoshogun@users.noreply.github.com>
2026-09-08 18:21:44 +0200

    test 2

--- Client/Program.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using Intersect.Framework.Reflection;
-
-var executingAssembly = Assembly.GetExecutingAssembly();
-Console.WriteLine(
-    $"Starting {executingAssembly.GetMetadataName()} in {Environment.CurrentDirectory}...\n\t{string.Join(' ', args)}"
-);
-
-Intersect.Client.Core.Program.Main(Assembly.GetExecutingAssembly(), args);
